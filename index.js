e62a3a218c61381c08f7de72ae5e05ce55b25957
Vignesh Kumar <vigneshuvi@gmail.com>
2017-01-18 10:40:56 +0530

    Integrated the create pin with image URL.

--- index.js
+++ index.js
@@ -26,70 +26,12 @@ var userMethods = {
         return response;
     },
 
-     /*
-     * Get User information based on Access token.
-     *
-     * @param  {JSON} request
-     * @return {JSON} response
-     */
-    getMe: function(request, response, data, callback) {
-        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.User};
-        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMe;
-        if (data) {
-            requestURL += '?' + querystring.stringify(data);
-        }
-        userMethods.get(requestURL, true, request, response, callback);
-    },
-
-     /*
-     * Get User information based on Access token.
-     *
-     * @param  {JSON} request
-     * @return {JSON} response
-     */
-    getMeBoards: function(request, response, data, callback) {
-        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.Boards};
-        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMeBoards;
-        if (data) {
-            requestURL += '?' + querystring.stringify(data);
-        }
-        userMethods.get(requestURL, true, request, response, callback);
-    },
-
-    /*
-     * Get User information based on Access token.
-     *
-     * @param  {JSON} request
-     * @return {JSON} response
-     */
-    getMeLikes: function(request, response, data, callback) {
-        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.Pins};
-        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMeLikes;
-        if (data) {
-            requestURL += '?' + querystring.stringify(data);
-        }
-        userMethods.get(requestURL, true, request, response, callback );
-    },
-
-     /*
-     * Get User information based on Access token.
-     *
-     * @param  {JSON} request
-     * @return {JSON} response
-     */
-    getMePins: function(request, response, data, callback) {
-        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.Pins};
-        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMePins;
-        if (data) {
-            requestURL += '?' + querystring.stringify(data);
-        }
-        userMethods.get(requestURL, true, request, response, callback);
-    },
-
     /* 
      * Method to make GET request
      *
      * @param string url
+     * @param Request req
+     * @param Response res
      * @param Function callback
      * @invoke callback(Object response)
      */
@@ -117,6 +59,116 @@ var userMethods = {
             return;
         });
     }
+
+
+    /* 
+     * Method to make POST request
+     *
+     * @param string url
+     * @param Request req
+     * @param Response res
+     * @param JSON data
+     * @param Function callback
+     * @invoke callback(Object response)
+     */
+    post: function(requestURL, shouldParse, req, res, data, callback) {
+        request.post({url: requestURL, form: data}, function(err, response, body){
+         /* ... */ 
+                if (err) {
+                    console.error('Error making GET request to endpoint ' + url);
+                    callback(req, res, shouldParse ? {} : "{}");
+                }
+
+                console.log("user response:"+body);
+                if (response && response.statusCode !== 200) {
+                    console.error('non 200 response for URL: ' + url);
+                    if (body) {
+                        callback(shouldParse ? JSON.parse(body) : body);
+                    } else {
+                        callback(req, res, shouldParse ? {} : "{}");
+                    }
+                    return;
+                } 
+
+                var toReturn = shouldParse ? JSON.parse(body) : body;
+                callback(req, res, toReturn);
+                return;
+            }
+        }
+    }
+
+     /*
+     * Get User information based on Access token.
+     *
+     * @param  {JSON} request
+     * @return {JSON} response
+     */
+    getMe: function(request, response, data, callback) {
+        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.User};
+        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMe;
+        if (data) {
+            requestURL += '?' + querystring.stringify(data);
+        }
+        userMethods.get(requestURL, true, request, response, callback);
+    },
+
+     /*
+     * Get User Boards based on Access token.
+     *
+     * @param  {JSON} request
+     * @return {JSON} response
+     */
+    getMeBoards: function(request, response, data, callback) {
+        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.Boards};
+        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMeBoards;
+        if (data) {
+            requestURL += '?' + querystring.stringify(data);
+        }
+        userMethods.get(requestURL, true, request, response, callback);
+    },
+
+    /*
+     * Get User Likes based on Access token.
+     *
+     * @param  {JSON} request
+     * @return {JSON} response
+     */
+    getMeLikes: function(request, response, data, callback) {
+        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.Pins};
+        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMeLikes;
+        if (data) {
+            requestURL += '?' + querystring.stringify(data);
+        }
+        userMethods.get(requestURL, true, request, response, callback );
+    },
+
+     /*
+     * Get User Pins based on Access token.
+     *
+     * @param  {JSON} request
+     * @return {JSON} response
+     */
+    getMePins: function(request, response, data, callback) {
+        var data = {"access_token":data.access_token, "fields": pinterestWSConfig.Fields.Pins};
+        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.GetMePins;
+        if (data) {
+            requestURL += '?' + querystring.stringify(data);
+        }
+        userMethods.get(requestURL, true, request, response, callback);
+    },
+
+    /*
+     * Post Pins based on Access token and image URL.
+     *
+     * @param  {JSON} request
+     * @return {JSON} response
+     */
+    postImageInBoard: function(request, response, data, callback) {
+        var requestURL = apiConfig.API_BASE_URL + pinterestWSConfig.WebServices.PostPins;
+        userMethods.get(requestURL, true, request, response, data, callback);
+    }
+
+
 };
  
 module.exports = userMethods;
